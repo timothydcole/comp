4834304117c6aafe1983a0f1cd7317c5e453fdc3
Steve <admin@cryptostat.org>
2018-10-29 14:34:40 +1100

    added index page and root

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :merchants
   resources :customers
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'application#index'
+
 end
