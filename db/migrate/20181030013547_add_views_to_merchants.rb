03185b261807de2cd86123a535e9a447526e40f2
Steve <admin@cryptostat.org>
2018-10-30 13:02:43 +1100

    added and installed  geocoder

--- db/migrate/20181030013547_add_views_to_merchants.rb
+++ db/migrate/20181030013547_add_views_to_merchants.rb
@@ -1,5 +1,7 @@
 class AddViewsToMerchants < ActiveRecord::Migration[5.2]
   def change
     add_column :merchants, :views, :integer, default: 0
+    add_column :merchants,[:latitude, :longitude]
+
   end
 end
