d21f60b5f609fbec5e7364fe8243d56b9b674b10
Steve <admin@cryptostat.org>
2018-11-08 18:25:00 +1100

    fix image_tag hate on Thomasgit add .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-   def image_tag(source, options={})
-          source = "blank_image.gif" if source.blank?
-        #   super(source, options)
-    end
-    
+   # def image_tag(source, options={})
+   #        source = "blank_image.gif" if source.blank?
+   #      #   super(source, options)
+   #  end
+
 end
